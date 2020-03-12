81cb8a41b150475385b68523625ff4ca037eb63a
Ryan Bennett <ry.nomad@protonmail.com>
2020-03-11 19:19:21 -0600

    add UI scaffolding

--- src/screens/location.js
+++ src/screens/location.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import { Container } from 'native-base'
-import {Header} from '../components/ui'
+import { Header } from '../components/ui'
 import { PermissionedLocation, SetGeofence, GetLocation, WaitLocation, WaitMovementStop, WaitGeofenceExit } from '../components/location'
-import {Text} from 'native-base'
+import { Text } from 'native-base'
 
+const GetLocationUI = ({ state: { context: { location } } }) => (
+    <>
+        <Text>Getting initial location</Text>
+        <Text>
+            {JSON.stringify(location)}
+        </Text>
+    </>
+)
+
+const WaitLocationUI = ({ state: { context: { location } } }) => (
+    <>
+        <Text>Waiting for Location</Text>
+        <Text>
+            {JSON.stringify(location)}
+        </Text>
+    </>
+)
+
+const WaitMovementStopUI = ({ state: { context: { location } } }) => (
+    <>
+        <Text>Outside of geofence, waiting for movement to stop</Text>
+        <Text>
+            {JSON.stringify(location)}
+        </Text>
+    </>
+)
+
+const WaitGeofenceExitUI = ({ state: { context: { location } } }) => (
+    <>
+        <Text>Waiting to Exit Geofence TAG 8:37</Text>
+        <Text>
+            {JSON.stringify(location)}
+        </Text>
+    </>
+)
 export default () => (
     <Container>
-        <Header 
+        <Header
             title="Location"
         />
         <PermissionedLocation>
             <GetLocation>
-                <Text>Getting initial location</Text>
+                <GetLocationUI />
             </GetLocation>
             <WaitLocation>
-                <Text>Waiting for Location TAG 8:37</Text>
+                <WaitLocationUI />
             </WaitLocation>
             <WaitGeofenceExit>
-                <Text>Waiting to Exit Geofence TAG 8:37</Text>
+                <WaitGeofenceExitUI />
             </WaitGeofenceExit>
             <WaitMovementStop>
-                <Text>Outside of geofence, waiting for movement to stop TAG 8:37</Text>
+                <WaitMovementStopUI />
             </WaitMovementStop>
         </PermissionedLocation>
     </Container>
