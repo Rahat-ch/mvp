ff65026026c810fe03448717fdfef681b7c31cba
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-03-21 19:27:24 -0400

    add welcome page

--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Text } from 'native-base';
-
+import { StyleSheet, Image, ImageBackground } from 'react-native';
+import { Container, Text, Body, Button } from 'native-base';
+import Cloud from '../images/welcome/Cloud.png';
+import Gradient from '../images/welcome/gradient.png';
 
 const Welcome = () => {
   return (
-    <Container style={styles.container}>
-      <Text style={styles.title}>CoEpi</Text>
+    <Container>
+      <ImageBackground
+        source={Gradient}
+        style={styles.container}
+      >
+        <Text style={styles.title}>CoEpi</Text>
+        <ImageBackground
+          source={Cloud}
+          style={styles.cloud}
+        >
+          <Text
+            style={styles.cloudText}
+          >
+            Contain, control, and eradicate contagious illnesses by connecting with your community.</Text>
+        </ImageBackground>
+        <Button>
+          <Text>Learn more</Text>
+        </Button>
+        <Button>
+          <Text>Skip</Text>
+
+        </Button>
+      </ImageBackground>
     </Container>
+
   )
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#86FFEA',
+    flex: 1,
   },
   title: {
     fontFamily: 'Roboto',
@@ -24,7 +47,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     letterSpacing: 1.5,
     color: '#FFFFFF',
-    marginTop: 80
+    marginTop: 80,
+  },
+  cloud: {
+    marginTop: 78,
+    marginLeft: 48,
+    position: "relative",
+    width: 327,
+    height: 173,
+  },
+  cloudText: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: "300",
+    fontSize: 14,
+    position: "absolute",
+    top: 75,
+    textAlign: "center"
   }
 })
 
